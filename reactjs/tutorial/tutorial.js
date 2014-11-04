4b2d10ba76b8cba62541c8bcdaaf5e0fea503d8b
Ben <hello@benshyong.com>
2014-11-03 18:37:14 -0800

    finish tutorial sec 8:

--- reactjs/tutorial/tutorial.js
+++ reactjs/tutorial/tutorial.js
@@ -19,7 +19,8 @@ var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="author One">a comment</Comment>
+        <Comment author="author Two">another comment</Comment>
       </div>
     );
   }
@@ -35,5 +36,27 @@ var CommentForm = React.createClass({
   }
 });
 
+var converter = new Showdown.converter();
+var Comment = React.createClass({
+  render: function(){
+    // props.children is the nested content
+    var rawMarkup = converter.makeHtml(this.props.children.toString());
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        // use this workaround to keep the html from being escaped
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+      </div>
+    );
+  }
+});
+
+
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
 
 
